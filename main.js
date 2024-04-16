516644c0b0f3a0a8c917b44c8945d4e74b1ce04b
davidearbola <davidearbola@gmail.com>
2024-04-16 15:02:27 +0200

    creazione prompt per creare password

---
+++ main.js
@@ -0,0 +1,20 @@
+let userName = prompt("Qual è il tuo nome?");
+
+let userSurname = prompt("Qual è il tuo cognome?");
+
+let favouriteColor = prompt("Qual è il tuo colore preferito?");
+
+let currentYear = prompt("In che anno siamo?");
+
+let userPass = userName + userSurname + favouriteColor + "#" + currentYear;
+
+// codice per scrivere direttamente il risultato dei prompt nel documento:
+document.writeln(userPass); 
+
+
+
+
+
+
+
+
